7d197e9d4125496e6a82b450c5f54d5dca7732fc
Ricardo Musch <ricardomusch@gmail.com>
2023-10-19 05:26:54 +0100

    Python 3 / PySide2 Compatibility

--- python/app/output_item.py
+++ python/app/output_item.py
@@ -11,42 +11,44 @@
 import os
 import tank
 from tank.platform.qt import QtCore, QtGui
-              
+
+
 class OutputItem(QtGui.QWidget):
     """
     """
+
     def __init__(self, output, parent=None):
         """
         Construction
         """
         QtGui.QWidget.__init__(self, parent)
-    
+
         self._output = output
-    
+
         # set up the UI
         from .ui.output_item import Ui_OutputItem
-        self._ui = Ui_OutputItem() 
+        self._ui = Ui_OutputItem()
         self._ui.setupUi(self)
-        
+
         self._update_ui()
-        
+
     @property
     def output(self):
         return self._output
-        
+
     @property
     def selected(self):
         return self._ui.select_cb.isChecked()
-        
+
     def mousePressEvent(self, event):
         if self._ui.select_cb.isEnabled():
             self._ui.select_cb.setChecked(not self._ui.select_cb.isChecked())
-        
+
     def _update_ui(self):
         """
         Update UI
         """
-        
+
         # set label text
         lines = []
         lines.append("<b>%s</b>" % self._output.display_name)
@@ -59,8 +61,7 @@ class OutputItem(QtGui.QWidget):
             icon = QtGui.QPixmap(icon_path)
             if not icon.isNull():
                 self._ui.icon_label.setPixmap(icon)
-        
+
         # selected state
         self._ui.select_cb.setChecked(self._output.selected)
         self._ui.select_cb.setEnabled(not self._output.required)
-        
